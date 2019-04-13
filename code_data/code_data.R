608a9cce6c1a838ca7a4bb11e58989311e1f7665
George Moroz <agricolamz@gmail.com>
2019-04-13 05:08:52 +0300

    add some slides

--- code_data/code_data.R
+++ code_data/code_data.R
@@ -3,3 +3,16 @@ library(qrcode)
 qrcode_gen('tinyurl.com/y6jjp38y')
 qrcode_gen('tinyurl.com/y6jjp38y', wColor = "#0099CC", bColor = "white")
 
+
+# data --------------------------------------------------------------------
+library(tidyverse)
+library(grid)
+library(gtable)
+
+theme_set(theme_bw())
+options(scipen=999)
+
+residence_langs <- read_csv("residence_langs_ND.csv")
+residence_langs %>% 
+  distinct(status)
+  
