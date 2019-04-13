0cc70d6ac70ba83478a7d996bc470310c2b21f45
George Moroz <agricolamz@gmail.com>
2019-04-13 03:50:54 +0300

    add code

---
+++ code_data/code_data.R
@@ -0,0 +1,5 @@
+# qrcodes -----------------------------------------------------------------
+library(qrcode)
+qrcode_gen('tinyurl.com/y6jjp38y')
+qrcode_gen('tinyurl.com/y6jjp38y', wColor = "#0099CC", bColor = "white")
+
